3d53bde72f0119cb89b5ebe85e095ff3220e77fc
Kodenko-N <kodenko.n@gmail.com>
2025-02-15 23:42:42 +0300

    PP_2_2_1 (One-to-One Hibernate Spring, User+Car) V.1.0.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -22,14 +22,20 @@ public class UserDaoImp implements UserDao {
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
+//      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+//      return query.getResultList();
+      return sessionFactory.getCurrentSession().createQuery("from User").getResultList();
    }
 
    public List<User> findUserByCarModelAndSeries(String carModel, int series) {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User u where u.car.model = :carModel and u.car.series = :series");
-      query.setParameter("carModel", carModel);
-      query.setParameter("series", series);
-      return query.getResultList();
+//      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User u where u.car.model = :carModel and u.car.series = :series");
+//      query.setParameter("carModel", carModel);
+//      query.setParameter("series", series);
+//      return query.getResultList();
+      return sessionFactory.getCurrentSession()
+              .createQuery("from User u where u.car.model = :carModel and u.car.series = :series")
+              .setParameter("carModel", carModel).setParameter("series", series)
+              .getResultList();
    }
+
 }
